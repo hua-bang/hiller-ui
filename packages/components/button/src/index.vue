45f8ec6ce71fe3fef650f28553ebb8f9f909c5aa
hua-bang <1964760211@qq.com>
2021-08-08 23:05:11 +0800

    feat: Button component

---
+++ packages/components/button/src/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <button 
+    class="hl-button"
+    @click="handleClick"
+    :class="classes"
+    :disabled="disabled"
+  >
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "HlButton",
+  emits: ["click"],
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    },
+    size: {
+      type: String
+    },
+    disabled: Boolean,
+    loading: Boolean,
+    round: Boolean
+  },
+  setup(props, context) {
+    
+    const handleClick = () => {
+      context.emit("click");
+    };
+
+    const classes = useClasses(props);
+
+    return {
+      handleClick,
+      classes
+    };
+  }
+};
+
+function useClasses(props) {
+  return computed(() => {
+    const { type, size, disabled, round } = props;
+    return [
+      type ? `hl-button--${type}` : "",
+      size ? `hl-button--${size}` : "",
+      disabled ? "is-disable": "",
+      round ? "is-round" : ""
+    ];
+  });
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+@import "@styles/button.scss";
+</style>
