cf66786cd2dcbb1543a3c7d85e8e94dbe5eb85fe
hua-bang <1964760211@qq.com>
2021-08-10 23:01:38 +0800

    feat: Card components

---
+++ packages/components/card/src/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+  class="hl-card"
+  :class="shadow ? `is-${shadow}-shadow`: 'is-always-shadow'"
+  >
+    <div class="hl-card--header" v-if="$slots.header || header">
+      <slot name="header">{{header}}</slot>
+    </div>
+    <div class="hl-card--body" :style="bodyStyle">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HlCard",
+  props: {
+    header: {},
+    bodyStyle: {},
+    shadow: {
+      type: String
+    }
+  },
+  setup() {
+
+  }
+};
+</script>
+
+<style>
+
+</style>
