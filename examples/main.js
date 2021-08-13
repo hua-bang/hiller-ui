60b62272b62fe2fc4e64eb02536271586a75408c
hua-bang <1964760211@qq.com>
2021-08-13 17:05:03 +0800

    feat: first build lib

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+
+import HillerUI from "../lib/hiller-ui.min";
+// import HillerUI from "../packages/index";
 import App from "./App.vue";
 import router from "./router";
 import "@styles/index.scss";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(HillerUI).use(router).mount("#app");
